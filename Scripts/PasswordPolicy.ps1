51f78455c10950d4797ef70247e33b0b513f20ba
jamalcopeland <jamalcopeland@outlook.com>
2025-10-23 16:02:44 -0400

    Update PasswordPolicy.ps1

--- Scripts/PasswordPolicy.ps1
+++ Scripts/PasswordPolicy.ps1
@@ -6,6 +6,12 @@
 ⚠️ MUST be run as Administrator.
 This script assumes elevated rights and does NOT check for them.
 
+.DESCRIPTION
+Configures Windows password policy settings for the following Windows 10 STIG requirements:
+WN10-AC-000020	The password history must be configured to 24 passwords remembered.
+WN10-AC-000030	The minimum password age must be configured to at least 1 day.
+WN10-AC-000035 Passwords must, at a minimum, be 14 characters
+
 .NOTES
     Author          : Jamal Copeland
     LinkedIn        : linkedin.com/in/jamalcopeland/
